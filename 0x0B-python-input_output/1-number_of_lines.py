1d38d571c7263add7b687c4a6a1b97dacd77eec2
Damilare Olawaye <olawayedamilare17@gmail.com>
2023-01-11 05:39:46 +0100

    Update 1-number_of_lines.py

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
+"""
+print number of lines of a file
 """
 
 
 def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-    n_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            n_lines += 1
-    return n_lines
+    sum = 0
+    with open(filename) as fd:
+        for line in fd:
+            sum += 1
+    return (sum)
